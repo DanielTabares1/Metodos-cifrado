1373e96972f0bae055f21da0ede248150f824e06
DanielTabares1 <64021707+DanielTabares1@users.noreply.github.com>
2022-10-11 11:37:39 -0500

    Update afin.py

--- algoritmos/afin.py
+++ algoritmos/afin.py
@@ -1,3 +1,4 @@
+#Se realiza el cifrado afin con la fórmula y = (7x+3) mod 30 y con llave de descifrado x = (13y +21) mod 30 
 simbolos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,_.?";
 mensaje = "KXBELJMY?DJMDMQLYLJ";
 decoded_message = "";
