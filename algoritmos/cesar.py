f182a3d51ee0fa74c7ea9822ba58475e2b3c9c21
DanielTabares1 <64021707+DanielTabares1@users.noreply.github.com>
2022-10-11 11:29:12 -0500

    Update cesar.py
    Se añade un comentario con información acerca del cifrado

--- algoritmos/cesar.py
+++ algoritmos/cesar.py
@@ -1,3 +1,4 @@
+#Se realiza el cifrado cesar con +25 mod 30 como unidad de desplazamiento, la llave para descifrar es +5 mod 30 
 simbolos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,_.?";
 mensaje = ",P?IJNW.DZNWZWOJ.JN";
 decoded_message = "";
